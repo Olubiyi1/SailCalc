fd0b1f253ec1feba1e95960b844495d5c351c968
Olubiyi Babajide <olubiyibabajide@gmail.com>
2024-09-10 17:04:15 +0100

    using div method

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("home")
+
+let display = document.getElementById('display');
+console.log(display);
+
+let toDisplay = (number)=>{
+    return display.textContent += number
+}
+
+let clr = ()=>{
+    display.textContent = " "
+}
+
+let del = ()=>{
+   
+}
