adc72ec86344f34e36f91f8f16913aee7d0561e9
olubiyi babajide <olubiyibabajide@gmail.com>
2024-09-11 17:39:16 +0100

    calculator done

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-console.log("home")
-
 let display = document.getElementById('display');
-console.log(display);
+let toShow = document.getElementById("collect")
+
 
 let toDisplay = (number)=>{
-    return display.textContent += number
+   if (display.textContent.length >= 25) {
+    alert("number too long,advised to clear")
+   }
+    return display.textContent += number;
 }
 
 let clr = ()=>{
-    display.textContent = " "
+    display.textContent = " ";
+    toShow.textContent = " "
+
 }
 
 let del = ()=>{
-   
+   display.textContent = display.innerText.slice(0,-1)
 }
+
+let calculate = ()=>{
+    toShow.textContent = eval(display.textContent)
+    // display.textContent =eval(display.textContent);
+}
+
